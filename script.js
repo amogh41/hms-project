0b6f9bb40d4e2ff27dad295d6d80825f1393577d
amogh41 <149044392+amogh41@users.noreply.github.com>
2026-05-26 12:20:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,37 @@
-const SUPABASE_URL = "https://ekqzqlamjalakueuozxv.supabase.co";
-const SUPABASE_KEY = "sb_publishable_Yso3unAMOzjoLNHVxqxmkw_SJ17c2qH";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const API_URL = "https://hms-project-j1fz.onrender.com/patients";
 
 async function addPatient() {
-    const name = document.getElementById("name").value;
-    const age = document.getElementById("age").value;
-    const disease = document.getElementById("disease").value;
-    const phone = document.getElementById("phone").value;
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let disease = document.getElementById("disease").value;
+    let phone = document.getElementById("phone").value;
 
     if (!name || !age || !disease || !phone) {
         alert("Please fill all fields");
         return;
     }
 
-    const { error } = await supabase
-        .from("patients")
-        .insert([
-            {
-                name: name,
-                age: age,
-                disease: disease,
-                phone: phone
-            }
-        ]);
+    await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, age, disease, phone })
+    });
 
-    if (error) {
-        console.log(error);
-        alert("Error saving patient");
-    } else {
-        alert("Patient Saved Successfully");
-        loadPatients();
-
-        document.getElementById("name").value = "";
-        document.getElementById("age").value = "";
-        document.getElementById("disease").value = "";
-        document.getElementById("phone").value = "";
-    }
+    loadPatients();
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("disease").value = "";
+    document.getElementById("phone").value = "";
 }
 
 async function loadPatients() {
-
-    const { data, error } = await supabase
-        .from("patients")
-        .select("*");
-
-    const table = document.getElementById("patientTable");
-
+    const res = await fetch(API_URL);
+    const patients = await res.json();
+    let table = document.getElementById("patientTable");
     table.innerHTML = "";
-
-    data.forEach(patient => {
-
-        table.innerHTML += `
-            <tr>
-                <td>${patient.name}</td>
-                <td>${patient.age}</td>
-                <td>${patient.disease}</td>
-                <td>${patient.phone}</td>
-            </tr>
-        `;
-    });
+    for (let p of patients) {
+        table.innerHTML += `<tr><td>${p.name}</td><td>${p.age}</td><td>${p.disease}</td><td>${p.phone}</td></tr>`;
+    }
 }
 
 loadPatients();
