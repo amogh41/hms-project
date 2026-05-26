9237f4df745588e924a20a7877b4fe6061fa1ace
Apple <apple@Apples-MacBook-Air.local>
2026-05-26 12:15:38 +0530

    Add CORS support

--- src/main/java/controller/PatientApiController.java
+++ src/main/java/controller/PatientApiController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
+@CrossOrigin(origins = "*")
 public class PatientApiController {
 
     private final PatientRepository patientRepository;
